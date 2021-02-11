b3d8797e7a6acf34bba48bbdbcf26c719c51ee58
axiaokun <3098995701@qq.com>
2021-02-11 11:03:18 +0800

    add:extract github config from code to properties

---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AccessTokenDto;
+import com.example.demo.dto.GithubUser;
+import com.example.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        accessTokenDto.setState(state);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(accessToken);
+        System.out.println(user);
+        return "index";
+    }
+}
